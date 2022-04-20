da6f159342e9b20a5c5003ba5c07fb1416ca6518
devklucas <lucascardoso64@gmail.com>
2022-04-20 10:42:28 -0300

    styles: style from version mobile registerpage

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ButtonStyled from './styles'
 
-const Button = ({text,...rest}) => {
+const Button = ({children,...rest}) => {
     return (
-        <ButtonStyled type="submit" {...rest}>{text}</ButtonStyled>
+        <ButtonStyled type="submit" {...rest}>{children}</ButtonStyled>
     )
 }
 export default Button 
